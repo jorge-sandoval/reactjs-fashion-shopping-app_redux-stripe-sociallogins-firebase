4be0fc87f4f751fd4663d3220377f9d237d2676e
Jorge O. Sandoval <jsandoval@comunidadandina.org>
2020-01-29 10:30:27 -0500

    First Commit. Home Page

---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home-page.styles.scss';
+import Directory from  '../../components/directory/directory.component'
+
+
+const HomePage = () => {    
+    return (
+        <div className="homepage">
+            <Directory></Directory>
+        </div>
+    )
+}
+
+export default HomePage;
