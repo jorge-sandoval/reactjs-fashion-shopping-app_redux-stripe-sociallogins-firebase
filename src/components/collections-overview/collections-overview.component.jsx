0230dd3ea85b9eb0504399bd52ef5df9f443348f
Jorge O. Sandoval <jsandoval@comunidadandina.org>
2020-02-04 16:52:49 -0500

    CollectionsPreview Styles

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import './collections-overview.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect'
 import { selectShopCollections } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../collection-preview/collection-preview.component'
 
-
 const CollectionsOverview = ({collections}) => {
     return (
-        <div>
+        <div className="collections-overview">
             {
                 collections.map(
                     ({ id, title, items }) => (
