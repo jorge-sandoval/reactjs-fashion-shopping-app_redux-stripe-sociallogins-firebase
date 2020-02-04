5c342f3bd3b3952fb0a06fd1bec1ca77ed71e450
Jorge O. Sandoval <jsandoval@comunidadandina.org>
2020-02-04 17:56:15 -0500

    Blank CategoryPage

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './category.styles.scss'
+import CollectionItem from '../../components/collection-item';
+
+const CategoryPage = () => {
+    return (
+        <div className="category-page">
+            <h2>CATEGORY PAGE</h2>
+        </div>
+    );
+}
+
+export default CategoryPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = () => {
+const ShopPage = ( { match } ) => {
     return ( 
-        <CollectionsOverview/>
+        <div className="shop-page">
+            <Route exact path={match.path} component={CollectionsOverview}/>
+        </div>
     )
 }
 
