8fad584625c4b2c10a3dd2c1db084015751096dc
Jorge O. Sandoval <jsandoval@comunidadandina.org>
2020-02-05 17:11:05 -0500

    ShopPage Child routes

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,16 @@
+  
+import React from 'react';
+import { connect } from 'react-redux';
+import './collection.styles.scss';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+const CollectionPage = () => {
+  return (
+    <div className='collection-page'>
+        <h2>COLLECTION</h2>
+    </div>
+  );
+};
+
+export default CollectionPage;
