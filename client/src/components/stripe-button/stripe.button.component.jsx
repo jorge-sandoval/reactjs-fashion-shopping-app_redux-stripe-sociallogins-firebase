66ab4f1e64aef34bbb36fcf16efd397a6785bc49
Jorge O. Sandoval <jsandoval@comunidadandina.org>
2020-04-09 02:44:33 -0500

    Stripe call

--- client/src/components/stripe-button/stripe.button.component.jsx
+++ client/src/components/stripe-button/stripe.button.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ( { price } )=> {
     const priceInCents = price * 100;
     const publishableKey = 'pk_test_MajIoSvYXRUGGY1hrcXDD1N200hDawGFvf'
 
-    const onToken = token => {
-        console.log(token);
-        alert('Paymenent Succesfull');
+    const onToken = ( token ) => {
+        axios({
+            url: 'http://localhost:5000/payment',
+            method: 'post',
+            data: {
+                amount: priceInCents,
+                token
+            }
+        }).then( response => {
+            alert('Payment successful');
+        }).catch( error => {
+            console.log( 'Payment error', error );
+            alert('There was an issue with your paiment');
+        });
     }
 
     return ( 
         <StripeCheckout 
             label="Pay Now"
             name="CROWN Clothing Ltd."
-            billingAddress
-            shippingAddress
             image='https://svgshare.com/i/CUz.svg'
             description={`Your total is $${price}`}
             amount={priceInCents}
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+var logger = require('logger').createLogger('development.log');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+logger.info(process.env.STRIPE_SECRET_KEY)
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,9 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-const configureRoutes = require('./routes');
+// const configureRoutes = require('./routes');
 
-configureRoutes(app);
+// configureRoutes(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
@@ -29,4 +33,22 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+});
+
+
+app.post('/payment', ( req, res ) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  }
+
+  stripe.charges.create(body, (stripeErr, stripeRes)=> {
+    if ( stripeErr ) {
+      logger.info(stripeErr);
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  })
 });
